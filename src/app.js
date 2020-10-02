83f87011a218d8e6b2ea3c5732744a9ff9cd8af1
nriccio <nicolasriccio78@gmail.com>
2020-10-02 13:23:57 +0200

    debug

--- src/app.js
+++ src/app.js
@@ -1,80 +1,56 @@
-// import de express
 import express from 'express'
+import { calc } from './calc.js'
 
-// definition de notre app
 const app = express()
-const router = express.Router()
 
-// Créer une programme app.js qui utilise express. Cette application devra tourner sur votre ip locale et le port 7777. Avec votre navigateur en se connectant sur en http à votre ip locale sur le port 7777 on devra récupérer le message 'Exercices express partie 2' Pour cela il faudra créer une route qui manage le path /
-const IP = '192.168.1.23'
+const IP_LOCAL = '192.168.0.11'
 const PORT = 7777
 
-app.get('/', (req, res) => {
-    res.send('Exercices express partie 2')
-})
+const sendMessage = (req, res) => {
+    res.send(req.message)
+}
 
-// Ajouter une route /get_current_time qui retournera à l'utilisateur la date du serveur sur lequel s'exécute app.js
-// app.get(`/get_current_time`, (req, res) => {
-//     const date = new Date()
-//     const log = `${date.toUTCString()}`
-//     res.send(`Nous somme le : ${log}`)
-// })
-
-// Ajouter une route /how_pass_data qui retourne à l'utilisateur un message qui lui explique comment on passe des données d'un handler/middleware à un autre sur express
-// app.get('/how_pass_data', (req, res) => {
-//     res.send(`Voir le cours`)
-// })
-
-const timer = (req, res, next) => {
-    const date = new Date()
-    req.requestDate = date.toUTCString()
-    res.send(req.requestDate)
+const wrapWithHtml = (req, res, next) => {
+    const html = `
+<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <title>Exercices express 2</title>
+      </head>
+    
+      <body>
+        <p>${req.message}</p>
+      </body>
+    </html>
+`
+    req.message = html
     next()
 }
 
-app.use('/get_current_time', timer)
-
-const useMiddleware = (req, res, next) => {
-    res.send(
-        `<a href="https://expressjs.com/fr/guide/using-middleware.html">Utilisation de middleware</a>
-        <p>Pour passer des données d'un middleware à l'autre nous utilisons la variable nommé next. Exemple : <div><code>var app = express();
-
-        app.use(function (req, res, next) {
-          console.log('Time:', Date.now());
-          next();
-        });</code></div></p>
-        `
-    )
+app.get('/', (req, res, next) => {
+    req.message = `Exercices express partie 2`
     next()
-}
-
-app.use('/how_pass_data', useMiddleware)
-
-//Number 5 = echec
-/*router.get(`/`, function (req, res) {
-    res.sendFile(
-        path.join(
-            '/mnt/c/Users/nricc/desktop/projets/exercices-node-express2/src' +
-                '/index.html'
-        )
-    )
 })
 
-router.get(`/get_current_time`, function (req, res) {
-    res.sendFile(
-        path.join(
-            '/mnt/c/Users/nricc/desktop/projets/exercices-node-express2/src' +
-                '/get_current_time'
-        )
-    )
+app.get('/get_current_time', (req, res, next) => {
+    const date = new Date().toUTCString()
+    req.message = date
+    next()
 })
 
-router.get(`/how_pass_data`, function (req, res) {
-    res.sendFile(path.join('/src/' + '/how_pass_data.html'))
-})*/
+app.get('/how_pass_data', (req, res, next) => {
+    const msg =
+        'On peut passer des données entre les middleware grâce aux obj req, res'
+    req.message = msg
+    next()
+})
 
-app.use('/', router)
-app.listen(PORT, IP, () => {
-    //exécution d'un affichage au lacement du serveur.
-    console.log(`Example app listening at http://${IP}:${PORT}`)
+app.use('/calc', calc)
+
+app.use(wrapWithHtml)
+app.use(sendMessage)
+// start the server
+app.listen(PORT, IP_LOCAL, () => {
+    console.log(`listening at ${IP_LOCAL}:${PORT}`)
 })
